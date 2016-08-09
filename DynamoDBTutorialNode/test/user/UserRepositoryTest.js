a32efe9174988af3c14bb3c5da4c36bd5bd8ba42
gkatzioura <gkatzioura@gmail.com>
2016-08-09 16:25:22 +0300

    Added atomic counter and conditional updates example

--- DynamoDBTutorialNode/test/user/UserRepositoryTest.js
+++ DynamoDBTutorialNode/test/user/UserRepositoryTest.js
@@ -4,11 +4,12 @@
 
 var UserRepository = require('../../user/UserRepository'),
 	TableCreator = require('../../TableCreator'),
+	async = require('async'),
 	assert = require('assert');
 
 describe('User Repository Test',function(){
     
-	before(function(done) {
+	beforeEach(function(done) {
 		TableCreator.deleteUsers(function(err,result) {
 			
 			if(err) {
@@ -39,6 +40,92 @@ describe('User Repository Test',function(){
 	
 	it('Query user',function(done) {
 		UserRepository.getUser("john@doe.com",function(err,result) {
+			if(err) {
+				done(err);
+			} else {
+				console.log(result);
+				done();
+			}
+		});
+	});
+	
+	it('Update user',function(done) {
+		
+		UserRepository.updateName("john@doe.com","Different Name",function(err,result) {
+			if(err) {
+				done(err);
+			} else {
+				UserRepository.getUser("john@doe.com",function(err,result) {
+					if(err) {
+						done(err);
+					} else {
+						assert.equal(result.Items[0].fullname,"Different Name");
+						done();
+					}
+				});
+			}	
+		});
+	});
+	
+	it('Update user conditionally',function(done) {
+		
+		UserRepository.updateConditionally("john@doe.com","Different Name","John",function(err,result) {
+			if(err) {
+				done(err);
+			} else {
+				UserRepository.getUser("john@doe.com",function(err,result) {
+					if(err) {
+						done(err);
+					} else {
+						assert.equal(result.Items[0].fullname,"Different Name");
+						done();
+					}
+				});
+			}	
+		});
+	});
+	
+	it('Add and increse counter',function(done){
+		
+		async.waterfall(
+				[
+				 function(callback) {
+					UserRepository.addUpdateCounter("john@doe.com",function(err,result){
+						if(err) {
+							callback(err);
+						} else {
+							callback();
+						}
+					});
+				 },
+				 function(callback) {
+					UserRepository.updateAndIncreaseCounter("john@doe.com","New Name",function(err,result){
+						if(err) {
+							callback(err);
+						} else {
+							callback();
+						}
+					});
+				 },
+				 function(callback) {
+				    UserRepository.getUser("john@doe.com",callback);
+				 }
+						     
+					 ],
+		function(err,result){
+			if(err) {
+				done(err);
+			} else {
+				assert.equal(result.Items[0].fullname,"New Name");
+				assert.equal(result.Items[0].counter,"1");
+				
+				done();
+			}
+		});
+	});
+	
+	it("Get Register Date",function(done) {
+		UserRepository.getRegisterDate("john@doe.com",function(err,result) {
 			if(err) {
 				done(err);
 			} else {
--- DynamoDBTutorialNode/user/UserRepository.js
+++ DynamoDBTutorialNode/user/UserRepository.js
@@ -24,6 +24,111 @@ var insertUser = function(email,fullName,registerDate,callback) {
 	dynamodb.putItem(params,callback);
 };
 
+var updateName = function(email,fullName,callback) {
+	
+	var docClient = new AWS.DynamoDB.DocumentClient();
+	
+	var params = {
+			TableName:"Users",
+			Key: {
+				email : email
+			},
+			UpdateExpression: "set fullname = :fullname",
+		    ExpressionAttributeValues:{
+		        ":fullname":fullName
+		    },
+		    ReturnValues:"UPDATED_NEW"
+		};
+	
+	docClient.update(params,callback);
+}
+
+var updateConditionally = function(email,fullName,prefix,callback) {
+	
+	var docClient = new AWS.DynamoDB.DocumentClient();
+	
+	var params = {
+			TableName:"Users",
+			Key: {
+				email : email
+			},
+			UpdateExpression: "set fullname = :fullname",
+			ConditionExpression: "begins_with(fullname,:prefix)",
+			ExpressionAttributeValues:{
+		        ":fullname":fullName,
+		        ":prefix":prefix
+		    },
+		    ReturnValues:"UPDATED_NEW"
+		};
+	
+	docClient.update(params,callback);
+}
+
+var addUpdateCounter = function(email,callback) {
+	
+	var docClient = new AWS.DynamoDB.DocumentClient();
+	
+	var params = {
+			TableName:"Users",
+			Key: {
+				email : email
+			},
+			UpdateExpression: "set #counter = :counter",
+			ExpressionAttributeNames:{
+		        "#counter":"counter"
+		    },
+			ExpressionAttributeValues:{
+		        ":counter":0
+		    },
+			ReturnValues:"UPDATED_NEW"
+		};
+	
+	docClient.update(params,callback);
+}
+
+var updateAndIncreaseCounter = function(email,fullName,callback) {
+
+	var docClient = new AWS.DynamoDB.DocumentClient();
+	
+	var params = {
+			TableName:"Users",
+			Key: {
+				email : email
+			},
+			UpdateExpression: "set fullname = :fullname ADD #counter :incva",
+			ExpressionAttributeNames:{
+		        "#counter":"counter"
+		    },
+			ExpressionAttributeValues:{
+		        ":fullname":fullName,
+		        ":incva":1
+		    },
+		    ReturnValues:"UPDATED_NEW"
+		};
+	
+	docClient.update(params,callback);
+}
+
+
+var getRegisterDate = function(email,callback) {
+	
+	var docClient = new AWS.DynamoDB.DocumentClient();
+	
+	var params = {
+		    TableName: "Users",
+		    KeyConditionExpression: "#email = :email",
+		    ExpressionAttributeNames:{
+		        "#email": "email"
+		    },
+		    ExpressionAttributeValues: {
+		        ":email":email
+		    },
+		    ProjectionExpression: 'registerDate'
+		};
+	
+	docClient.query(params,callback);
+}
+
 var getUser = function(email,callback) {
 	
 	var docClient = new AWS.DynamoDB.DocumentClient();
@@ -44,5 +149,10 @@ var getUser = function(email,callback) {
 
 module.exports = {
 	insertUser:insertUser,
-	getUser: getUser
+	getUser: getUser,
+	getRegisterDate: getRegisterDate,
+	updateName: updateName,
+	updateConditionally: updateConditionally,
+	addUpdateCounter: addUpdateCounter,
+	updateAndIncreaseCounter: updateAndIncreaseCounter
 }
