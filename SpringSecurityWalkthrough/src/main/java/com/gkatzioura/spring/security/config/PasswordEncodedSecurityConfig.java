11603e0d3f366780d3653df7ffd9ff4e352ee817
gkatzioura <gkatzioura@gmail.com>
2016-10-11 09:39:17 +0300

    Changed to bcrypt

--- SpringSecurityWalkthrough/src/main/java/com/gkatzioura/spring/security/config/PasswordEncodedSecurityConfig.java
+++ SpringSecurityWalkthrough/src/main/java/com/gkatzioura/spring/security/config/PasswordEncodedSecurityConfig.java
@@ -2,11 +2,11 @@ package com.gkatzioura.spring.security.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Profile;
-import org.springframework.security.authentication.encoding.Md5PasswordEncoder;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 import javax.sql.DataSource;
 
@@ -24,7 +24,7 @@ public class PasswordEncodedSecurityConfig extends WebSecurityConfigurerAdapter 
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 
         auth.jdbcAuthentication().dataSource(dataSource)
-                .passwordEncoder(new Md5PasswordEncoder())
+                .passwordEncoder(new BCryptPasswordEncoder())
                 .usersByUsernameQuery("SELECT username,password,1 FROM Custom_Users_Encoded_pass where username=?")
                 .authoritiesByUsernameQuery("SELECT username,authority FROM Custom_Roles where username=?");
     }
