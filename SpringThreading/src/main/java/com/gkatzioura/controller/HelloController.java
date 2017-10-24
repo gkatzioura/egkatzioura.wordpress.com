33576364715d612f70e0ae8931cab85dcf7b1fc0
gkatzioura <gkatzioura@gmail.com>
2017-10-24 23:31:19 +0300

    Added request body

--- SpringThreading/src/main/java/com/gkatzioura/controller/HelloController.java
+++ SpringThreading/src/main/java/com/gkatzioura/controller/HelloController.java
@@ -6,6 +6,7 @@ import com.gkatzioura.service.AsynchronousService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -42,7 +43,7 @@ public class HelloController {
     }
 
     @RequestMapping(value = "/employee",method = RequestMethod.POST)
-    public void add(Employee employee) {
+    public void add(@RequestBody Employee employee) {
 
         employeeRepository.save(employee);
     }
