2280ea43136388951d2282f32d2a13e474ff46ac
gkatzioura <gkatzioura@gmail.com>
2020-02-25 23:45:33 +0000

    Firt commit for Spring Cloud gateway

---
+++ cloud-gateway-poc/src/main/java/com/gkatzioura/cloudgatewaypoc/CloudGatewayPocApplication.java
@@ -0,0 +1,13 @@
+package com.gkatzioura.cloudgatewaypoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudGatewayPocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudGatewayPocApplication.class, args);
+	}
+
+}
