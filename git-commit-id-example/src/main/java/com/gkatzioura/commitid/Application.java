850930b3bdb1adadaf087e3dd7a1bf8059e05430
gkatzioura <gkatzioura@gmail.com>
2021-07-28 00:00:43 +0100

    Added repo

---
+++ git-commit-id-example/src/main/java/com/gkatzioura/commitid/Application.java
@@ -0,0 +1,14 @@
+package com.gkatzioura.commitid;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        ConfigurableApplicationContext context = SpringApplication.run(Application.class, args);
+    }
+
+}
